216c7b9de2c6c535d44ac7368af12985cda545ce
Yaroslav Yermilov <yermilov17@gmail.com>
2017-02-25 15:23:54 +0200

    add page parameter

--- articles_sort_webserver.py
+++ articles_sort_webserver.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 
 @app.route('/sort/<user_id>')
 def sort(user_id):
-    request = requests.get('http://pocket_square_articles:8080/article/byUserId/' + user_id + '/unread')
+    request = requests.get('http://pocket_square_articles:8080/article/byUserId/' + user_id + '/unread?page=0&size=20')
     response = request.json()
 
     new_response = []
