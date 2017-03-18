73cb18b09dcfe6052434ec25bc28b0d133355264
Yaroslav Yermilov <yermilov17@gmail.com>
2017-03-18 13:01:07 +0200

    try to implement configurable environment

--- articles_sort_webserver.py
+++ articles_sort_webserver.py
@@ -3,6 +3,15 @@ from flask import request as flask_request
 import requests
 import json
 from random import shuffle
+import os
+import yaml
+
+with open("application.yml", 'r') as ymlfile:
+    cfg = yaml.load(ymlfile)
+
+environment = os.environ['SERVICE_ENVIRONMENT']
+if not environment:
+    environment = cfg['default_environment']
 
 app = Flask(__name__)
 
@@ -10,7 +19,7 @@ app = Flask(__name__)
 @app.route('/sort/<user_id>')
 def sort(user_id):
     count = flask_request.args.get('count')
-    request = requests.get('http://188.166.174.189:28103/article/byUserId/%s/unread?page=0&size=%s' % (user_id, count))
+    request = requests.get('%s/article/byUserId/%s/unread?page=0&size=%s' % (cfg[environment]['pocket_square_articles_service'], user_id, count))
     response = request.json()
 
     new_response = []
@@ -21,4 +30,4 @@ def sort(user_id):
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5001, debug=True)
+    app.run(host='0.0.0.0', port=cfg[environment]['port'], debug=cfg[environment]['debug'])
