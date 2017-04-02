4068bda387d0bf8c1c1ac09d358969671bd183e4
Vlad Grytsun <gritsun@users.noreply.github.com>
2017-04-02 12:10:06 -0700

    allow CORS

--- articles_sort_webserver.py
+++ articles_sort_webserver.py
@@ -5,6 +5,7 @@ import json
 from random import shuffle
 import os
 import yaml
+from flask_cors import CORS, cross_origin
 
 with open("application.yml", 'r') as ymlfile:
     cfg = yaml.load(ymlfile)
@@ -17,16 +18,21 @@ else:
 app = Flask(__name__)
 
 
+# http://localhost:5002/sort/58b1800dc9e77c0001d1d702?count=10
 @app.route('/sort/<user_id>')
+@cross_origin()
 def sort(user_id):
+    print 'RECEIVED REQUEST'
     count = request.args.get('count')
     articles_request = requests.get('%s/article/byUserId/%s/unread?page=0&size=%s' % (cfg[environment]['pocket_square_articles_service'], user_id, count))
     response = articles_request.json()
 
     new_response = []
     for k in response:
+        del k["content"]
         new_response.append(k)
     shuffle(new_response)
+    print 'SENT RESPONSES'
     return json.dumps(new_response)
 
 
